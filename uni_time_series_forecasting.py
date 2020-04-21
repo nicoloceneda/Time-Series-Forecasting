d7fdbea01fbf13e5dcf5f21de6155604416a8df5
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-04-21 19:04:33 +0200

    Add files via upload

--- uni_time_series_forecasting.py
+++ uni_time_series_forecasting.py
@@ -1,6 +1,10 @@
 """ UNIVARIATE TIME SERIES FORECASTING
     ----------------------------------
     Implementation of a lstm recurrent neural network for univariate time series forecasting.
+
+    This script uses a weather time series dataset, which contains 14 features collected every 10 minutes between 2009 and 2016.
+
+    Code reference: https://www.tensorflow.org/tutorials/structured_data/time_series#single_step_model
 """
 
 
@@ -13,6 +17,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
+
+
+# Set the seed
+
+tf.random.set_seed(1)
 
 
 # -------------------------------------------------------------------------------
@@ -28,24 +37,24 @@ print(climate.head())
 
 # Select only one feature
 
-ds = climate['T (degC)']
-ds.index = climate['Date Time']
-print(ds.head())
+uni_data = climate['T (degC)']
+uni_data.index = climate['Date Time']
+print(uni_data.head())
 
-ds = ds.values
+uni_data = uni_data.values
 
 
 # Standardize the features using the parameters of the training set
 
 train_split = 300000
-ds_train_mean = ds[:train_split].mean()
-ds_train_std = ds[:train_split].std()
-ds_std = (ds - ds_train_mean) / ds_train_std
+uni_train_mean = uni_data[:train_split].mean()
+uni_train_std = uni_data[:train_split].std()
+uni_data_std = (uni_data - uni_train_mean) / uni_train_std
 
 
-# Generate the train and valid subsets
+# Create features and targets for train and valid subsets
 
-def dataset_generator(dataset, start_index, end_index, history_len, target_len):
+def uni_dataset_generator(dataset, start_index, end_index, history_len, target_len):
 
     end_index = len(dataset) - target_len if end_index is None else end_index
 
@@ -54,9 +63,12 @@ def dataset_generator(dataset, start_index, end_index, history_len, target_len):
 
     for i in range(start_index, end_index - history_len):
 
-        history_index = range(i, i + history_len)  # range(start_index, start_index + history_size) | range(end_index - 1 - history_size, end_index - 1)
+        # range(start_index, start_index + history_size) | range(end_index - 1 - history_size, end_index - 1)
+        history_index = range(i, i + history_len)
         data.append(np.reshape(dataset[history_index], (history_len, 1)))
-        labels.append(dataset[i + history_len + target_len])  # (start_index + history_size + target_size) | (end_index - 1 + target_size)
+
+        # (start_index + history_size + target_size) | (end_index - 1 + target_size)
+        labels.append(dataset[i + history_len + target_len])
 
     return np.array(data), np.array(labels)
 
@@ -64,20 +76,19 @@ def dataset_generator(dataset, start_index, end_index, history_len, target_len):
 history_len = 20
 target_len = 0
 
-x_train_std, y_train_std = dataset_generator(ds_std, 0, train_split, history_len, target_len)
-x_valid_std, y_valid_std = dataset_generator(ds_std, train_split, None, history_len, target_len)
+uni_x_train_std, uni_y_train_std = uni_dataset_generator(uni_data_std, 0, train_split, history_len, target_len)
+uni_x_valid_std, uni_y_valid_std = uni_dataset_generator(uni_data_std, train_split, None, history_len, target_len)
 
 
 # Cache, shuffle and batch the train and valid subsets
 
 batch_size = 256
-tf.random.set_seed(1)
 
-ds_train = tf.data.Dataset.from_tensor_slices((x_train_std, y_train_std))
-ds_train = ds_train.cache().shuffle(10000).batch(batch_size).repeat()
+uni_train_std = tf.data.Dataset.from_tensor_slices((uni_x_train_std, uni_y_train_std))
+uni_train_std = uni_train_std.cache().shuffle(10000).batch(batch_size).repeat()
 
-ds_valid = tf.data.Dataset.from_tensor_slices((x_valid_std, y_valid_std))
-ds_valid = ds_valid.batch(batch_size).repeat()
+uni_valid_std = tf.data.Dataset.from_tensor_slices((uni_x_valid_std, uni_y_valid_std))
+uni_valid_std = uni_valid_std.batch(batch_size).repeat()
 
 
 # -------------------------------------------------------------------------------
@@ -87,7 +98,7 @@ ds_valid = ds_valid.batch(batch_size).repeat()
 
 # Create a function to plot the history, true value and model prediction
 
-def show_plot(plot_data, delta, title):
+def plot_prediction(plot_data, delta, title):
 
     labels = ['History', 'True Future', 'Model Prediction']
     marker = ['.-', 'bx', 'rx']
@@ -113,28 +124,17 @@ def show_plot(plot_data, delta, title):
     return plt
 
 
-# Design a baseline model which predicts the next point as the average of the last 20 observations
-
-def baseline(history):
-
-    return np.mean(history)
-
-
-plot = show_plot([x_train_std[0], y_train_std[0], baseline(x_train_std[0])], 0, 'Baseline Prediction Example')
-plot.show()
-
-
 # Design the lstm recurrent neural network
 
 simple_lstm_model = tf.keras.models.Sequential()
-simple_lstm_model.add(tf.keras.layers.LSTM(units=8, input_shape=x_train_std.shape[-2:]))
+simple_lstm_model.add(tf.keras.layers.LSTM(units=8, input_shape=uni_x_train_std.shape[-2:]))
 simple_lstm_model.add(tf.keras.layers.Dense(1))
 
 
 # Print the model summary
 
 simple_lstm_model.summary()
-print('Input shape:', x_train_std.shape[-2:])
+print('Input shape:', uni_x_train_std.shape[-2:])
 
 
 # Compile the model to specify optimizer, loss function
@@ -144,7 +144,7 @@ simple_lstm_model.compile(optimizer='adam', loss='mae')
 
 # Make a sample prediction to check the output of the model
 
-for x, y in ds_valid.take(1):
+for x, y in uni_valid_std.take(1):
 
     print('Prediction shape:', simple_lstm_model.predict(x).shape, '\n')
 
@@ -156,16 +156,16 @@ for x, y in ds_valid.take(1):
 
 # Train the lstm recurrent neural network
 
-history = simple_lstm_model.fit(ds_train, epochs=10, steps_per_epoch=200, validation_data=ds_valid, validation_steps=50)
+history = simple_lstm_model.fit(uni_train_std, epochs=10, steps_per_epoch=200, validation_data=uni_valid_std, validation_steps=50)
 
 
 # Now that you have trained your simple LSTM, let's try and make a few predictions
 
-for x, y in ds_valid.take(3):
+for x, y in uni_valid_std.take(3):
 
     prediction = simple_lstm_model.predict(x)
 
-    plot = show_plot([x[0].numpy(), y[0].numpy(), prediction[0]], 0, 'Simple LSTM model')
+    plot = plot_prediction([x[0].numpy(), y[0].numpy(), prediction[0]], 0, 'Simple LSTM model')
     plot.show()
 
 
